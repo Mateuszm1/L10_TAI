9d210be8ba782dfa4f1d1b71d6f02dcbd74637bd
Marvel <codebreaker.mw@gmail.com>
2021-05-24 23:14:43 +0200

    lab10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,32 @@ import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
 import {BlogDetailsComponent} from "./components/blog-details/blog-details.component";
 import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
-
+import {AuthGuard} from "./services/auth.guard";
+import{LoginComponent} from "./components/login/login.component";
+import{SignupComponent} from "./components/signup/signup.component";
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
   {
+	path: 'login', 
+	component: LoginComponent
+	},
+	{
+	path: 'signup',
+	component: SignupComponent
+   },
+  {
     path: 'contact',
     component: ContactComponent,
+	
   },
   {
     path: 'blog',
     component: BlogHomeComponent,
+	canActivate: [AuthGuard]
+	
   },
   {
     path: 'blog/detail/:id',
